399873205c69b5a4d9c3180a11ec7d16a4fb5ffa
kishore15062006 <kishore15062006@gmail.com>
2026-06-17 21:16:50 +0530

    second

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/farm_management_portal/",
+  base: "/",
   server: {
     host: "::",
     port: 8080,
